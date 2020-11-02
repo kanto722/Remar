b1011edbd27425b5d331ce1015757c003b4581e5
Kanto Kanto72 <kanto72@yandex.ru>
2020-11-02 09:52:37 +0300

    adaptiv 768

--- js/slider.js
+++ js/slider.js
@@ -1,11 +1,23 @@
 const slider = document.querySelector('.swiper-container');
 
 let mySwiper = new Swiper(slider, {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 24,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
     //   loop: true,
+    lazy: {
+      loadPrevNext: true,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+
+      }
+    }
   })
